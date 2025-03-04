f3f1f43d5831c1af2fd3657cafe15adbbf735448
seanflynn04 <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2025-03-03 20:54:17 -0700

    updates

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -40,6 +40,28 @@ architecture Behavioral of sevenseg_decoder is
 
 begin
 
-
+with i_Hex select
+o_seg_n <=  "0000001" when "0000",
+            "1001111" when "0001",
+            "0010010" when "0010",
+            "0000110" when "0011",
+            "1001100" when "0100",
+            "0100100" when "0101",
+            "0100000" when "0110",
+            "0001111" when "0111",
+            "0000000" when "1000",
+            "0001100" when "1001",
+            "0001000" when "1010",
+            "1100000" when "1011",
+            "1110010" when "1100",
+            "1000010" when "1101",
+            "0100000" when "1110",
+            "0111000" when "1111",
+            "1111111" when others;
 
 end Behavioral;
+
+
+
+
+
